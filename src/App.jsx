b83a5f26ade73d54f5607f5cd70d55d35597aabd
Mohammed Aahil <mdaahil86@gmail.com>
2026-06-06 11:35:23 +0530

    Phase 1: Fetch users with TanStack

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const App = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      if (!res.ok) {
+        throw new Error("Failed to fetch");
+      }
+
+      console.log(data);
+      return res.json();
+    },
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading users</p>;
+  console.log(data);
+
   return <div>User Explorer</div>;
 };
 
